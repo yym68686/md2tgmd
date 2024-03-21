83e3a86b82593695bff25ee9806578c6de2d1474
yym68686 <yym68686@outlook.com>
2024-03-21 14:42:23 +0800

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="md2tgmd",
-    version="0.1.0",
+    version="0.1.1",
     description="md2tgmd is a Markdown to Telegram-specific-markdown converter.",
     long_description=Path.open(Path("README.md"), encoding="utf-8").read(),
     long_description_content_type="text/markdown",
